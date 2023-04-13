62009ecb39ace791a6361c5a2fdcc8d13e8e0b57
jiebao <jie.bao@cae.com>
2023-04-13 09:38:46 -0400

    fix main_backtranslate with new main

--- main/main_backtranslate.py
+++ main/main_backtranslate.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 from tqdm import tqdm
 from transformers import MarianMTModel, MarianTokenizer
+from multiprocessing import Pool
 
 from src.data.text_processing import TextPreprocessor
 from src.utils import defines
@@ -16,65 +17,85 @@ def main() -> None:
 
     # Load train
     train = pd.read_csv(Path(defines.INTERIM_DATA_DIR, "train.csv"))
-    columns_train = train.columns
 
     # Apply backtranslate
     backtranslated_df = backtranslate_dataset(train)
 
+    # Remove undesired special tokens
+    backtranslated_df_cleaned = remove_special_tokens(backtranslated_df, 'text')
+
     # save to csv
-    df = pd.DataFrame(backtranslated_df)
+    df = pd.DataFrame(backtranslated_df_cleaned)
     output_path = os.path.join(Path(defines.DATA_DIR), 'backtranslate_all.csv')
     df.to_csv(output_path, index=False)
 
 
-# Create a function to backtranslate an entire dataset
 def backtranslate_dataset(dataset):
+    """
+        This function backtranslates the text in the given dataset using multiple processes for faster execution.
+        It utilizes Python's multiprocessing.Pool to parallelize the backtranslation process across multiple CPU cores.
 
-    backtranslated_dataset = []
-
-    for idx in tqdm(range(len(dataset)), desc="Backtranslating"):
-        # Get the targets
-
-        rewire_id = dataset.iloc[idx]['rewire_id']
-        target_a = dataset.iloc[idx]['target_a']
-        target_b = dataset.iloc[idx]['target_b']
-        target_c = dataset.iloc[idx]['target_c']
-
-        # Backtranslate the text
-        backtranslated_text = backtranslate(dataset.iloc[idx]['text'])
-
-        # Create a new review with the backtranslated text and the same label
-        backtranslated_review = {'rewire_id': rewire_id, 'text': backtranslated_text,
-                                 'target_a': target_a,
-                                 'target_b': target_b,
-                                 'target_c': target_c}
-
-        # Add the backtranslated review to the list
-        backtranslated_dataset.append(backtranslated_review)
-
+        :param dataset: pd.DataFrame: A pandas DataFrame containing the dataset with a 'text' column to backtranslate.
+        :return: list of dicts: backtranslated text and target labels for each tasks and the rewired ID.
+    """
+    with Pool(processes=6) as pool:
+        backtranslated_dataset = list(tqdm(pool.imap(backtranslate_example, dataset.iterrows()),
+                                           total=len(dataset),
+                                           desc="Backtranslating"))
     return backtranslated_dataset
 
+def backtranslate_example(row):
+    idx, data = row
+
+    rewire_id = data['rewire_id']
+    target_a = data['target_a']
+    target_b = data['target_b']
+    target_c = data['target_c']
+
+    backtranslated_text = backtranslate(data['text'])
+
+    backtranslated_review = {'rewire_id': rewire_id, 'text': backtranslated_text,
+                             'target_a': target_a,
+                             'target_b': target_b,
+                             'target_c': target_c}
+    return backtranslated_review
 
 def backtranslate(text, source_lang='en', target_lang='fr'):
-    # Initialize the tokenizer and model for translation to the target language
-    tokenizer_to = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}')
-    model_to = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}')
+    try:
+        # Initialize the tokenizer and model for translation to the target language
+        tokenizer_to = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}')
+        model_to = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}')
 
-    # Tokenize the text and translate to the target language
-    encoded = tokenizer_to.encode(text, return_tensors='pt')
-    translated = model_to.generate(encoded)
-    target_text = tokenizer_to.decode(translated[0])
+        # Tokenize the text and translate to the target language
+        encoded = tokenizer_to.encode(text, return_tensors='pt')
+        translated = model_to.generate(encoded)
+        target_text = tokenizer_to.decode(translated[0])
 
-    # Initialize the tokenizer and model for translation back to the original language
-    tokenizer_back = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{target_lang}-{source_lang}')
-    model_back = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{target_lang}-{source_lang}')
+        # Initialize the tokenizer and model for translation back to the original language
+        tokenizer_back = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{target_lang}-{source_lang}')
+        model_back = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{target_lang}-{source_lang}')
 
-    # Tokenize the target language text and translate back to the original language
-    encoded = tokenizer_back.encode(target_text, return_tensors='pt')
-    back_translated = model_back.generate(encoded)
-    backtranslated_text = tokenizer_back.decode(back_translated[0])
+        # Tokenize the target language text and translate back to the original language
+        encoded = tokenizer_back.encode(target_text, return_tensors='pt')
+        back_translated = model_back.generate(encoded)
+        backtranslated_text = tokenizer_back.decode(back_translated[0])
 
-    return backtranslated_text
+        return backtranslated_text
+
+    except Exception as e:
+        print(f"Error during backtranslation: {e}")
+        print(f"Original text: {text}")
+        return text
+
+
+def remove_special_tokens(df, column):
+    df[column] = df[column].apply(process_string)
+    return df
+
+
+def process_string(s):
+    s = s.lstrip('<pad>').rstrip('</s>')
+    return s.strip()
 
 
 if __name__ == "__main__":
