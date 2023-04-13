97eece1cf801105b42848e263cd8961b0a569092
axelbogos <bogosaxel@gmail.com>
2023-04-13 15:03:54 -0400

    switch pytorch-lightning for lightning

---
+++ main/main_training_localmodules.py
@@ -0,0 +1,191 @@
+import logging
+import pprint
+from argparse import Namespace
+
+import wandb
+from dotenv import load_dotenv
+from pytorch_lightning import Trainer, seed_everything
+
+from src.data.datamodule_transformer_local import DataModuleTransformerLocal
+from src.models.wrapper_transformer_module import WrapperTransformerModule
+from src.utils import defines, helpers
+
+
+def main(model_name: str):
+    """The main function is the entry point of the program.
+
+    It initializes all necessary variables and objects, then starts training.
+    """
+    assert model_name in defines.VALID_MODEL_CHOICES, f"Invalid model name: {model_name}"
+    # Load environment variables
+    load_dotenv(defines.DOTENV_FILE)
+
+    # Setup logging & parse args
+    log_dir = helpers.make_log_dir()
+    helpers.setup_python_logging(log_dir)
+
+    args_task_a = {
+        "train": True,
+        "eval": True,
+        "task": "a",
+        "architecture": "transformer",
+        "model": model_name,
+        "dropout": 0.1,
+        "optimizer": "AdamW",
+        "lr": 5e-06,
+        "step_scheduler": 5,
+        "n_warmup_steps": 0,
+        "preprocessing_mode": "none",
+        "max_token_length": 128,
+        "batch_size": 16,
+        "num_epoch": 9,
+        "patience": 3,
+        "num_workers": 1,
+        "random_seed": 3454572,
+        "logs_dir": defines.LOG_DIR,
+        "raw_data_dir": defines.RAW_DATA_DIR,
+        "interim_data_dir": defines.INTERIM_DATA_DIR,
+        "processed_data_dir": defines.PROCESSED_DATA_DIR,
+        "log_dir": log_dir,
+    }
+    args_task_b = args_task_a.copy()
+    args_task_c1 = args_task_a.copy()
+    args_task_c2 = args_task_a.copy()
+    args_task_c3 = args_task_a.copy()
+    args_task_c4 = args_task_a.copy()
+    args_task_b["task"] = "b"
+    args_task_c1["task"] = "c"
+    args_task_c2["task"] = "c"
+    args_task_c3["task"] = "c"
+    args_task_c4["task"] = "c"
+    args_task_a = Namespace(**args_task_a)
+    args_task_b = Namespace(**args_task_b)
+    args_task_c1 = Namespace(**args_task_c1)
+    args_task_c2 = Namespace(**args_task_c2)
+    args_task_c3 = Namespace(**args_task_c3)
+    args_task_c4 = Namespace(**args_task_c4)
+    args_task_a.log_dir = log_dir
+    args_wrapper = {
+        "train": True,
+        "eval": True,
+        "task": "multitask",
+        "architecture": "transformer-wrapper",
+        "model": model_name,
+        "dropout": 0.1,
+        "optimizer": "AdamW",
+        "lr": 5e-06,
+        "step_scheduler": 5,
+        "n_warmup_steps": 0,
+        "preprocessing_mode": "none",
+        "max_token_length": 128,
+        "batch_size": 16,
+        "num_epoch": 9,
+        "patience": 3,
+        "num_workers": 1,
+        "random_seed": 3454572,
+        "logs_dir": defines.LOG_DIR,
+        "raw_data_dir": defines.RAW_DATA_DIR,
+        "interim_data_dir": defines.INTERIM_DATA_DIR,
+        "processed_data_dir": defines.PROCESSED_DATA_DIR,
+        "log_dir": log_dir,
+    }
+    args_wrapper = Namespace(**args_wrapper)
+
+    # Setup WandB logging
+    # wandb_logger = helpers.setup_wandb(args_wrapper)
+
+    logger = logging.getLogger(__name__)
+    seed_everything(args_task_a.random_seed)
+
+    # Build data module
+    data_module_task_a = DataModuleTransformerLocal(subtask="a")
+    data_module_task_b = DataModuleTransformerLocal(subtask="b")
+    data_module_task_c1 = DataModuleTransformerLocal(subtask="c1")
+    data_module_task_c2 = DataModuleTransformerLocal(subtask="c2")
+    data_module_task_c3 = DataModuleTransformerLocal(subtask="c3")
+    data_module_task_c4 = DataModuleTransformerLocal(subtask="c4")
+
+    data_module_task_a.setup()
+    data_module_task_b.setup()
+    data_module_task_c1.setup()
+    data_module_task_c2.setup()
+    data_module_task_c3.setup()
+    data_module_task_c4.setup()
+
+    args_task_a.num_target_class = data_module_task_a._num_classes
+    args_task_a.len_train_loader = len(data_module_task_a.train_dataloader())
+    args_task_b.num_target_class = data_module_task_b._num_classes
+    args_task_b.len_train_loader = len(data_module_task_b.train_dataloader())
+    args_task_c1.num_target_class = data_module_task_c1._num_classes
+    args_task_c1.len_train_loader = len(data_module_task_c1.train_dataloader())
+    args_task_c2.num_target_class = data_module_task_c2._num_classes
+    args_task_c2.len_train_loader = len(data_module_task_c2.train_dataloader())
+    args_task_c3.num_target_class = data_module_task_c3._num_classes
+    args_task_c3.len_train_loader = len(data_module_task_c3.train_dataloader())
+    args_task_c4.num_target_class = data_module_task_c4._num_classes
+    args_task_c4.len_train_loader = len(data_module_task_c4.train_dataloader())
+
+    # Build models
+    model_task_a = helpers.get_model(args_task_a)
+    model_task_b = helpers.get_model(args_task_b)
+    model_task_c1 = helpers.get_model(args_task_c1)
+    model_task_c2 = helpers.get_model(args_task_c2)
+    model_task_c3 = helpers.get_model(args_task_c3)
+    model_task_c4 = helpers.get_model(args_task_c4)
+
+    lightning_callbacks = helpers.get_lightning_callbacks(args_task_a)
+
+    trainer_a = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_a.num_epoch,
+    )
+    trainer_b = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_b.num_epoch,
+    )
+    trainer_c1 = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_c1.num_epoch,
+    )
+    trainer_c2 = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_c2.num_epoch,
+    )
+    trainer_c3 = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_c3.num_epoch,
+    )
+    trainer_c4 = Trainer(
+        logger=logger,
+        callbacks=lightning_callbacks,
+        accelerator="auto",
+        devices="auto",
+        max_epochs=args_task_c4.num_epoch,
+    )
+
+    # Train
+    trainer_a.fit(model_task_a, datamodule=data_module_task_a)
+    trainer_b.fit(model_task_b, datamodule=data_module_task_b)
+    trainer_c1.fit(model_task_c1, datamodule=data_module_task_c1)
+    trainer_c2.fit(model_task_c2, datamodule=data_module_task_c2)
+    trainer_c3.fit(model_task_c3, datamodule=data_module_task_c3)
+    trainer_c4.fit(model_task_c4, datamodule=data_module_task_c4)
+
+
+if __name__ == "__main__":
+    main("distilroberta-base")
---
+++ src/data/datamodule_transformer_local.py
@@ -0,0 +1,197 @@
+from pathlib import Path
+from typing import Optional
+
+# import pytorch_lightning as pl
+import lightning as pl
+import pandas as pd
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer
+
+from src.data.components.Dataset import GenericDatasetTransformer
+from src.data.text_processing import TextPreprocessor
+from src.utils import defines
+
+
+class DataModuleTransformerLocal(pl.LightningDataModule):
+    def __init__(
+        self,
+        subtask,
+        model="distilroberta-base",
+        preprocessing_mode="none",
+        batch_size=16,
+        num_workers=1,
+    ):
+        """The __init__ function is called when the class is instantiated. It sets up the instance
+        of the class, and defines all its attributes.
+
+        :param self: Represent the instance of the class
+        :param args: Pass the arguments from the command line to the class
+        :return: None
+        """
+        super().__init__()
+        self.model = model
+        self.preprocessing_mode = preprocessing_mode
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.data_train: Optional[GenericDatasetTransformer] = None
+        self.data_val: Optional[GenericDatasetTransformer] = None
+        self.data_test: Optional[GenericDatasetTransformer] = None
+
+        self.tokenizer = AutoTokenizer.from_pretrained(model)
+        self.subtask = subtask
+
+        # data preparation handlers
+        self.text_preprocessor = TextPreprocessor(preprocessing_mode=self.preprocessing_mode)
+
+    def setup(self, stage: Optional[str] = None):
+
+        """The setup function is called by lightning with both `trainer.fit()` and
+        `trainer.test()`, so be careful not to execute things like random split twice!
+
+        :param self: Represent the instance of the class
+        :param stage: Optional[str]: Determine whether the training is in train or test mode
+        :return: A tuple of the train, val and test datasets
+        """
+
+        if not self.data_train:
+            interim_data_train = pd.read_csv(Path(defines.INTERIM_DATA_DIR, "train.csv"))
+            interim_data_train["text"] = self.text_preprocessor.transform_series(
+                interim_data_train["text"]
+            )
+            interim_data_train = interim_data_train[interim_data_train[self._target_label] != -1]
+            interim_data_train = interim_data_train[
+                interim_data_train[self._target_label] in self._get_subtask_targets
+            ]
+            interim_data_train = interim_data_train.to_numpy()
+
+            self.data_train = GenericDatasetTransformer(
+                texts=interim_data_train[:, 1],
+                labels=interim_data_train[:, self._target_index],
+                tokenizer=self.tokenizer,
+                max_token_len=128,
+            )
+
+        if not self.data_val:
+            val_path = Path(defines.INTERIM_DATA_DIR, "val.csv")
+            interim_data_val = pd.read_csv(val_path)
+            interim_data_val["text"] = self.text_preprocessor.transform_series(
+                interim_data_val["text"]
+            )
+            interim_data_val = interim_data_val[interim_data_val[self._target_label] != -1]
+            interim_data_val = interim_data_val[
+                interim_data_val[self._target_label] in self._get_subtask_targets
+            ]
+            interim_data_val = interim_data_val.to_numpy()
+
+            self.data_val = GenericDatasetTransformer(
+                texts=interim_data_val[:, 1],
+                labels=interim_data_val[:, self._target_index],
+                tokenizer=self.tokenizer,
+                max_token_len=128,
+            )
+
+        if not self.data_test:
+            interim_data_test = pd.read_csv(Path(defines.INTERIM_DATA_DIR, "test.csv"))
+            interim_data_test["text"] = self.text_preprocessor.transform_series(
+                interim_data_test["text"]
+            )
+            interim_data_test = interim_data_test[interim_data_test[self._target_label] != -1]
+            interim_data_test = interim_data_test[
+                interim_data_test[self._target_label] in self._get_subtask_targets
+            ]
+            interim_data_test = interim_data_test.to_numpy()
+
+            self.data_test = GenericDatasetTransformer(
+                texts=interim_data_test[:, 1],
+                labels=interim_data_test[:, self._target_index],
+                tokenizer=self.tokenizer,
+                max_token_len=128,
+            )
+
+    def train_dataloader(self):
+        """The train_dataloader function is used to load the training data.
+
+        :param self: Represent the instance of the class
+        :return: A dataloader object that contains the training data
+        """
+        return DataLoader(
+            self.data_train,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        """The val_dataloader function is used to create a DataLoader object for the validation
+        set. The function takes no arguments and returns a DataLoader object that can be used to
+        iterate over the validation data.
+
+        :param self: Bind the instance of the class to the method
+        :return: A dataloader object that contains the validation data
+        """
+        return DataLoader(self.data_val, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    def test_dataloader(self):
+        """The test_dataloader function is used to load the test data.
+
+        :param self: Bind the attributes and methods of a class to the class object
+        :return: A dataloader object
+        """
+        return DataLoader(self.data_test, batch_size=self.batch_size, num_workers=self.num_workers)
+
+    @property
+    def _num_classes(self):
+
+        """The _num_classes function returns the number of classes for a given task.
+
+        :param self: Represent the instance of the class
+        :return: The number of classes in the dataset
+        """
+        return len(self._get_subtask_targets)
+
+    @property
+    def _target_index(self):
+
+        """The _target_index function returns the index of the target_col column in a training
+        dataframe.
+
+        :param self: Bind the instance of the class to a function
+        :return: The index of the target_col column in the training data
+        """
+        if "a" in self.subtask == "a":
+            return 2
+        elif "b" in self.subtask == "b":
+            return 3
+        elif "c" in self.subtask == "c":
+            return 4
+
+    @property
+    def _target_label(self):
+
+        """The _target_label function is used to determine the target_col label for training. The
+        function takes in a single argument, self, which is an instance of the class.
+
+        :param self: Bind the instance of the class to the method
+        :return: The label of the training set for current task
+        """
+        if "a" in self.subtask == "a":
+            return "target_a"
+        elif "b" in self.subtask == "b":
+            return "target_b"
+        elif "c" in self.subtask == "c":
+            return "target_c"
+
+    @property
+    def _get_subtask_targets(self):
+        if self.subtask == "a":
+            return [0, 1]
+        elif self.subtask == "b":
+            return [0, 1, 2, 3]
+        elif self.subtask == "c1":
+            return [0, 1]
+        elif self.subtask == "c2":
+            return [2, 3, 4]
+        elif self.subtask == "c3":
+            return [5, 6, 7, 8]
+        elif self.subtask == "c4":
+            return [9, 10]
