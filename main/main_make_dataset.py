645efa4838afc57dbf774cd7001a86a1c1cf2e13
jiebao <jie.bao@cae.com>
2023-04-28 18:13:17 -0400

    add task a

--- main/main_make_dataset.py
+++ main/main_make_dataset.py
@@ -8,23 +8,23 @@ def main():
     helpers.setup_python_logging()
     logger = logging.getLogger(__name__)
 
-    # # Download data
-    # logger.info("Downloading raw data...")
-    # downloader = raw_data_downloader.GoogleDriveDownloader(defines.RAW_DATA_DIR)
-    # downloader.download()
-    #
-    # # Encode and merge val & test set labels
-    # interim_processor = interim_preprocessing.InterimProcessor(
-    #     defines.RAW_DATA_DIR, defines.INTERIM_DATA_DIR
-    # )
-    # interim_processor.run()
+    # Download data
+    logger.info("Downloading raw data...")
+    downloader = raw_data_downloader.GoogleDriveDownloader(defines.RAW_DATA_DIR)
+    downloader.download()
 
-
-    # Augment data
-    data_augmentation_processor = data_augmentation_preprocessing.DataAugmentationProcessor(
-        defines.AUGMENTED_DATA_DIR, defines.AUGMENTED_DATA_DIR
+    # Encode and merge val & test set labels
+    interim_processor = interim_preprocessing.InterimProcessor(
+        defines.RAW_DATA_DIR, defines.INTERIM_DATA_DIR
     )
-    data_augmentation_processor.run()
+    interim_processor.run()
+
+
+    # # Augment data
+    # data_augmentation_processor = data_augmentation_preprocessing.DataAugmentationProcessor(
+    #     defines.AUGMENTED_DATA_DIR, defines.AUGMENTED_DATA_DIR
+    # )
+    # data_augmentation_processor.run()
 
 if __name__ == "__main__":
     main()
